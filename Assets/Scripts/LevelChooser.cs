51aac49e1ab355de44b966c86969dc9014e205fe
Ugniuks <ugne.kam2004@gmail.com>
2025-03-10 00:54:34 +0200

    updated the level selector
    the precious one didnt work

--- Assets/Scripts/LevelChooser.cs
+++ Assets/Scripts/LevelChooser.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelPicker : MonoBehaviour
+
+public class LevelChooser : MonoBehaviour
 {
     public void OpenLevel(int levelID)
     {
         string levelName = "Level" + levelID;
         SceneManager.LoadScene(levelName);
     }
+
 }
