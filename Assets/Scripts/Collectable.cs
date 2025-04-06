13f718e8e0ce85bce211c0bda4205cfadd673c88
emylyje <emiobe@ktu.lt>
2025-04-07 01:16:27 +0300

    collectable sound

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,12 +4,15 @@ public class Collectible : MonoBehaviour
 {
     public string itemName;
     public Sprite itemIcon;
+    public AudioClip collectSound;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Triggered by: " + other.gameObject.name);
         if (other.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(collectSound, Camera.main.transform.position);
+
             Debug.Log("Player detected. Adding item: " + itemName);
             InventoryManager.Instance.AddItem(this);
             Destroy(gameObject);
