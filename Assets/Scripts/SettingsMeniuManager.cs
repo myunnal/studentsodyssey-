292414c724ad1d092ef2f6b209eefe697a55024e
Ugniuks <ugne.kam2004@gmail.com>
2025-04-22 22:24:18 +0300

    Resolution added
    idejau resolution keitimus ir full screen, dar mini borden mygtuku patweakinimai

---
+++ Assets/Scripts/SettingsMeniuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMeniuManager : MonoBehaviour
+{
+    public TMP_Dropdown ResDropDown;
+    public Toggle FullScreen;
+
+    Resolution[] AllRez;
+    bool isFullScreen;
+    int SelectedResolution;
+
+    List<Resolution> SelectedRezList = new List<Resolution>();
+
+    private void Start()
+    {
+        isFullScreen = true;
+        AllRez = Screen.resolutions;
+
+        List<string> resolutionStringList = new List<string>();
+        string newRez;
+
+        foreach(Resolution res in AllRez)
+        {
+            newRez = res.width.ToString() + " x " + res.height.ToString();
+            if(!resolutionStringList.Contains(newRez))
+            {
+                resolutionStringList.Add(newRez);
+                SelectedRezList.Add(res);
+            }
+        }
+
+        ResDropDown.AddOptions(resolutionStringList);
+    }
+
+    public void ChangeResolution()
+    {
+        SelectedResolution = ResDropDown.value;
+        Screen.SetResolution(SelectedRezList[SelectedResolution].width, SelectedRezList[SelectedResolution].height, isFullScreen);
+    }
+
+    public void ChangeFullScreen()
+    {
+        isFullScreen = FullScreen.isOn;
+        Screen.SetResolution(SelectedRezList[SelectedResolution].width, SelectedRezList[SelectedResolution].height, isFullScreen);
+    }
+
+}
